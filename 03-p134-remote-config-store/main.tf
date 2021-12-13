4a54f37792af739f0483346ab6c5e664366df0b8
Aleksandr Rekun <rekusha@gmail.com>
2021-12-13 15:06:09 +0200

    s3 remote tfstate

---
+++ 03-p134-remote-config-store/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {                # 1 создаем бакет S3 ресурсом (дял хранения конфигурайций
+  bucket = "rekusha-terraform-up-and-running-state" # УНИКАЛЬНОЕ имя бакета
+
+  lifecycle {
+    prevent_destroy = true                          # предотвращает случайное удаление
+  }
+
+  versioning {
+    enabled = true                                  # включает версионирование файлов
+  }
+
+  server_side_encryption_configuration {            # включает шифрование содержимого указанным алгоритмом
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {           # 2 создаем таблицу в динамодб для реализации механизма блокировок файла состояния тераформа
+  hash_key = "LockID"       # первичный ключь
+  name     = "rekusha-terraform-up-and-running-state-locks" # имя таблицы
+  billing_mode = "PAY_PER_REQUEST" # режим работы таблицы и тарифа
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {                                                # 3 конфигурация самой системы Terraform
+  backend "s3" {                                        # s3 это имя хранилища, которое будем использовать
+    bucket = "rekusha-terraform-up-and-running-state"   # имя бакета где будет храниться
+    key = "global/s3/terraform.tfstate"                 # Файловый путь внутри бакета S3, по которому Terraform будет записывать файл состояния
+    region = "eu-west-2"                                # Регион AWS, в котором находится бакет S3
+    dynamodb_table = "rekusha-terraform-up-and-running-state-locks" # Таблица DynamoDB, которая будет использоваться для блокирования
+    encrypt = true                                      # Если указать true, состояние Terraform будет шифроваться при сохранении в S3
+  }
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
+}
+
